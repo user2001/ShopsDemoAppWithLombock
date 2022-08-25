0f23c58a4c58f65b06c2063680faf20fffb689a4
user2001 <ivanna.fryshchyn@gmail.com>
2022-08-25 17:22:33 +0300

    Exceptions added

---
+++ src/main/java/com/example/ShopsDemoApp/exception/ShopErrorResponse.java
@@ -0,0 +1,34 @@
+package com.example.ShopsDemoApp.exception;
+
+public class ShopErrorResponse {
+    private int status;
+    private String message;
+    private long timeStamp;
+
+    public ShopErrorResponse() {
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
